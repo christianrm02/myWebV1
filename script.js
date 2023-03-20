8c574306669dab09b15eed0813a746502fe1d93b
christian.rivero <christian.rivero@estudiantat.upc.edu>
2023-03-20 19:08:21 +0100

    changes

--- script.js
+++ script.js
@@ -29,11 +29,9 @@ load();
 blight.addEventListener('click', function() {
   if (body.classList.contains('light-mode')) { //oscuro
     body.classList.remove('light-mode');
-    body.classList.add('dark-mode');
     store(false);
     img_mode.src = "images/sun2.png";
   } else { //claro
-    body.classList.remove('dark-mode');
     body.classList.add('light-mode');
     store(true);
     img_mode.src = "images/moon.png";
@@ -49,7 +47,7 @@ if(fechaActual.getMonth() < 4 || (fechaActual.getMonth() == 4 && fechaActual.get
 }
 //MOSTRAT TEXTO PRESENTACIÓN
 const typed = new Typed('#mi_parrafo', {
-  strings: [`Buenas! Soy Christian Rivero, actualmente tengo ${edad} años, nací en Barcelona el 20 de abril del 2002 y soy Ingeniero informático, concretamente, de la especialidad de Ingeniería del Software, titulado en la UPC. Desde pequeño me han llamado mucho la atención los ordenadores, las consolas, los diseños de las páginas web, y sobre todo, el porqué de las cosas, me considero una persona muy curiosa y que le gusta aprender y conocer diferentes puntos de vista de todo tipo de persona. Soy trabajador, perfeccionista, perseverante, resolutivo y con capacidad de trabajar en equipo. Si estás leyendo esto es porque o estoy presumiendo de página web (está bien hecha eh <img src="/images/guiño3.png" alt="emoticono guiño" id="emote_guiño"/> ) o estás aquí interesado en mi y en mi portafolio. En ese caso, espero que te sirva esta web para conocerme un poco y más, y si necesitas conocerme mejor, contáctame por privado desde <a href="#bloque4" id="contacto_ref">aquí</a>.`],
+  strings: [`Buenas! Soy Christian Rivero Marin (CRM), actualmente tengo ${edad} años, nací en Barcelona el 20 de abril del 2002 y soy Ingeniero informático, concretamente, de la especialidad de Ingeniería del Software, titulado en la UPC. Desde pequeño me han llamado mucho la atención los ordenadores, las consolas, los diseños de las páginas web, y sobre todo, el porqué de las cosas, me considero una persona muy curiosa y que le gusta aprender y conocer diferentes puntos de vista de todo tipo de persona. Soy trabajador, perfeccionista, perseverante, resolutivo y con capacidad de trabajar en equipo. Si estás leyendo esto es porque o estoy presumiendo de página web (está bien hecha eh <img src="/images/guiño3.png" alt="emoticono guiño" id="emote_guiño"/> ) o estás aquí interesado en mi y en mi portafolio. En ese caso, espero que te sirva esta web para conocerme un poco y más, y si necesitas conocerme mejor, contáctame por privado desde <a href="#bloque4" id="contacto_ref">aquí</a>.`],
   typeSpeed: 15,
 });
 
@@ -96,6 +94,32 @@ observer.observe(document.querySelector('#cat'));
 observer.observe(document.querySelector('#ing'));
 observer.observe(document.querySelector('#fr'));
 observer.observe(document.querySelector('#jp'));
+
+but_sidebar_show = document.querySelector('#show_sidebar_button');
+sidebar = document.querySelector('.lateralOptOut');
+/*but_sidebar_show.addEventListener('click', function() {
+  if (body.classList.contains('hide_sidebar')) { //se muestra
+    body.classList.remove('hide_sidebar');
+  } else { //se oculta
+    body.classList.add('hide_sidebar');
+  }
+});*/
+
+but_sidebar_show.addEventListener('click', function() {
+  if (sidebar.style.transform === 'translateX(-100%)') { //se muestra
+    sidebar.style.transform = 'translateX(0%)';
+    body.classList.remove('hide_sidebar');
+  } else { //se oculta
+    sidebar.style.transform = 'translateX(-100%)';
+    body.classList.add('hide_sidebar');
+  }
+});
+
+
+if(screen.width < 767) {
+    sidebar.style.transform = 'translateX(-100%)';
+    body.classList.add('hide_sidebar');
+}
 
 /*
 <span id="edad"></span>
